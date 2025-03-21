440a95b434410be7902a802822976855bef30084
BRUNOSR-DEV <bruno77_sr@hotmail.com>
2025-03-21 12:36:43 -0300

    att style: formatação pagina, área projetos e mais

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib import admin
 
 from .views import IndexView
 
@@ -6,3 +7,7 @@ urlpatterns = [
     path('', IndexView.as_view(), name='index'),# ('') rota para raiz - IndexView.as_view() vai ser executada como uma função
     
 ]
+
+admin.AdminSite.site_header = 'Administração'
+admin.AdminSite.site_title = 'Portifólio'
+admin.AdminSite.index_title = 'Área Administrativa|Portifólio'
